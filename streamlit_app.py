f5a85768fa9d3d9f53362256de0a8f7041e778c8
wakh3e <223608238@edu.vut.ac.za>
2025-08-25 05:06:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,19 @@ with st.expander('Data visualization'):
   #"bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
+
+#Data preparation
+with st.sidebar:
+  st.header('Input features')
+  #"island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+
+  island = st.selectbox('Island', ('Bisco', 'Dream', 'Torgersen')
+  gender = st.selectbox('Gender', ('male', 'female'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.3, 59.6, 43.9)
+  bil_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)' , 2700.0, 6300.0, 4207.0
+
+
+
+
