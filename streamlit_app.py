af630a70350dd833f130cd28f6f88b438b74fea2
wakh3e <223608238@edu.vut.ac.za>
2025-08-25 05:10:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ with st.sidebar:
   bill_length_mm = st.slider('Bill length (mm)', 32.3, 59.6, 43.9)
   bil_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
   flipper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)
-  body_mass_g = st.slider('Body mass (g)' , 2700.0, 6300.0, 4207.0
+  body_mass_g = st.slider('Body mass (g)' , 2700.0, 6300.0, 4207.0)
 
 
 
